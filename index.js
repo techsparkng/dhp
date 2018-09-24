f32d2336f5efe44c3b8d5bae19829ef27ba15b86
Ayobamidele Ayeko <ayobamideleayeko@AyobamidelesMBP.Dual-Band-Broadband-Modem>
2018-09-24 23:25:02 +0100

    ...

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var express 	= require('express'),
-	app			= express(),
-	mongoose	= require('mongoose'),
-	path		= require('path'),
-	bodyParser 	= require('body-parser');
+var express = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	path = require('path'),
+	bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/views')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.sendFile('index.html');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log(`DHP app running at port: ${PORT}`);
 });
