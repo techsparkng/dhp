95c40070beabcb2bc91d07d4b7a6781e657f5632
Ayobamidele Ayeko <ayobamideleayeko@AyobamidelesMBP.Dual-Band-Broadband-Modem>
2018-10-02 02:01:18 +0100

    Final Update UI/BN

--- model/package.js
+++ model/package.js
@@ -3,19 +3,10 @@ var mongoose = require('mongoose'),
 
 var packageSchema = new mongoose.Schema({
     type: String,
-    interest: Number,
-    start: {
-        type: Date,
-        default: Date.now
-    },
-    end: {
-        type: Date,
-        default: Date.now
-    },
-    duration: {
-        type: Date,
-        default: Date.now
-    }
+    interest:Number,
+    start: Date,
+    end: Date,
+    duration: Date,
 });
 
 packageSchema.plugin(passportLocalMongoose);
